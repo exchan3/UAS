014778c6264288936452a65f3717c05374f86f60
exchan3 <63703295+exchan3@users.noreply.github.com>
2020-06-12 19:45:07 +0700

    Add files via upload

---
+++ halaman/session.php
@@ -0,0 +1,39 @@
+<?php
+ 
+    include 'koneksi.php';
+ 
+    session_start();
+ 
+    $query = mysqli_query($koneksi,"select * from tbl_soal WHERE aktif='Y'") or die (mysqli_error());
+ 
+    //$_SESSION['soal'] = mysql_fetch_array($query);
+ 
+    $_SESSION['soal'] = array();
+ 
+    $_SESSION['no'] = 1;
+ 
+    $_SESSION['score'] = 0;
+ 
+    $_SESSION['option'] = array();
+ 
+    $_SESSION['jawab'] = array();
+ 
+    $i=0;
+ 
+    while($row = mysqli_fetch_assoc($query)){
+ 
+        $_SESSION['soal'][] = $row;
+ 
+        $_SESSION['option'][] = array($_SESSION['soal'][$i]['a'], $_SESSION['soal'][$i]['b'], $_SESSION['soal'][$i]['c'], $_SESSION['soal'][$i]['d']);
+ 
+        $i++;
+ 
+    }
+ 
+    if(isset($_SESSION['soal'])){
+ 
+        header("location:index.php?page=soal");
+ 
+    }
+ 
+?>
