ea1c62b1d1ff02948bd9284fdb548d4226bda2b4
exchan3 <63703295+exchan3@users.noreply.github.com>
2020-06-29 20:45:50 +0700

    Update tambah_soal.php

--- admin/tambah_soal.php
+++ admin/tambah_soal.php
@@ -15,60 +15,63 @@
 		<tr>
 		<td>Masukan ID Soal</td>
 		<td>:</td>
-		<td><input type="text" name="idsoal" id="idsoal"></td>
+		<td><input type="text" name="id_soal" id="idsoal" required/></td>
 		</tr>
 		
 		<tr>
 		<td>Masukan Soal</td>
 		<td>:</td>
-		<td><textarea name="soal" id="soal"></textarea></td>
+		<td><textarea name="soal" id="soal" required/></textarea></td>
 		</tr>
 		
 		<tr>
 		<td>Pilihan A</td>
 		<td>:</td>
-		<td><input type="text" name="a" id="a"></td>
+		<td><input type="text" name="a" id="a" required/></td>
 		</tr>
 			
 		<tr>
 		<td>Pilihan B</td>
 		<td>:</td>
-		<td><input type="text" name="b" id="b"></td>
+		<td><input type="text" name="b" id="b" required/></td>
 		</tr>
 			
 		<tr>
 		<td>Pilihan C</td>
 		<td>:</td>
-		<td><input type="text" name="c" id="c"></td>
+		<td><input type="text" name="c" id="c" required/></td>
 		</tr>
 			
 		<tr>
 		<td>Pilihan D</td>
 		<td>:</td>
-		<td><input type="text" name="d" id="d"></td>
+		<td><input type="text" name="d" id="d" required/></td>
 		</tr>
 			
 		<tr>
 		<td>Kunci</td>
 		<td>:</td>
-		<td><input type="text" name="knc" id="knc"></td>
+		<td><input type="text" name="knc_jawaban" id="knc" required/></td>
 		</tr>
 			
 		<tr>
 		<td>Jawaban</td>
 		<td>:</td>
-		<td><input type="text" name="jwb" id="jwb"></td>
+		<td><input type="text" name="jawaban" id="jwb" required/></td>
 		</tr>
 		
 		<tr>
 		<td>Aktif</td>
 		<td>:</td>
-		<td><input type="radio" name="aktif" id="y" value="ya"><label>Yes</label>
-			<input type="radio" name="aktif" id="n" value="no"><label>No</label></td>
+		<td><input type="radio" name="aktif" id="aktif" value="Y"><label>Yes</label>
+			<input type="radio" name="aktif" id="aktif" value="N"><label>No</label></td>
 		</tr>
 			
 		<tr>
-		<td><input type="button" value="Submit"></td>	
+		<td><button type="submit">Tambah</button>
+			<a href="halaman_admin.php">Back</a>	
+		<td>	
+		
 		</tr>
 		
 			
